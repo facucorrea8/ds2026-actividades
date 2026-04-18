001985dc8c91e0fedac8864df85f20565bc40ae2
facundo correa <juancorrea051@gmail.com>
2026-04-18 12:03:32 -0300

    Ejercicio 1: Variables y Operaciones

---
+++ ejercicios/c03-javascript/ej1-variables-operaciones/app.js
@@ -0,0 +1,13 @@
+const nombre = "Facundo Correa";
+const edad = "22";
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log("El resultado del contador es:", contador );
